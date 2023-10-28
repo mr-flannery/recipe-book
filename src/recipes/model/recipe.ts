84cb6ddeb3ecb72c560b24cf5da0023e4e233440
Dennis Hempfing <dennis.hempfing@sap.com>
2023-10-28 11:29:47 +0200

    initial commit and then some

---
+++ src/recipes/model/recipe.ts
@@ -0,0 +1,14 @@
+export interface Recipe {
+  name: string;
+  prepTime: string;
+  cookTime: string;
+  ingredients: string;
+  instructions: string;
+  // tags: Tag[];
+} 
+
+// export interface Duration {
+//   hours: number;
+//   minutes: number;
+//   seconds: number;
+// }
